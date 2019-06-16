b5d5879d3055790c23c36a40c1f45a2ef6e4ccbe
yeonjuan <yeonjuan93@naver.com>
2019-06-16 22:16:42 +0900

    filter 인자명 변경

--- ProblemSolving/String/strong-password.js
+++ ProblemSolving/String/strong-password.js
@@ -2,7 +2,7 @@
 
 const minimumNumber = (length, password) => {
   const regExps = [/[a-z]/, /[A-Z]/, /[!@#$%^&*()\-+]/, /[0-9]/];
-  const unsatisfiedRegexCount = regExps.filter(regExps => !regExps.test(password)).length;
+  const unsatisfiedRegexCount = regExps.filter(regExp => !regExp.test(password)).length;
   const neededLength = 6 - length;
   return Math.max(unsatisfiedRegexCount, neededLength);
 };
