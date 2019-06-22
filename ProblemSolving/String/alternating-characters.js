3468335502ede0dc5118a000988a4b79f71d0d55
yeonjuan <yeonjuan93@naver.com>
2019-06-22 23:25:28 +0900

    alternating-characters

---
+++ ProblemSolving/String/alternating-characters.js
@@ -0,0 +1,14 @@
+// https://www.hackerrank.com/challenges/alternating-characters/problem
+
+const alternatingCharacters = (str) => {
+  const alteredString = alternateString(str);
+  return str.length - alteredString.length;
+};
+
+const alternateString = (str) => (
+  [...str].reduce((altered, cur) => (
+    altered.slice(-1) === cur
+      ? altered
+      : altered + cur
+  ))
+);
