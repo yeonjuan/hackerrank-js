dc361805cd0b7e81481ec4176940d0e6395fac70
yeonjuan <yeonjuan93@naver.com>
2019-06-22 23:36:40 +0900

    two strings

---
+++ ProblemSolving/String/two-strings.js
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/two-strings/problem
+
+const twoStrings = (str1, str2) => {
+  const set1 = new Set([...str1]);
+  const set2 = new Set([...str2]);
+  const intersectSet = intersect(set1, set2);
+
+  return intersectSet.size ? 'YES' : 'NO';
+};
+
+const intersect = (set1, set2) => new Set([...set1].filter(x => set2.has(x)));
