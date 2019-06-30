ddcc82b0e93bbd3753de274821363dfa94059106
Heejun Lee <zaqq1414@gmail.com>
2019-06-30 23:42:41 +0900

    #13-helloworld 문제풀이

---
+++ 10DaysOfJavascript/Day0/hello-world.js
@@ -0,0 +1,7 @@
+// https://www.hackerrank.com/challenges/js10-hello-world/problem
+
+function greeting(parameterVariable) {
+    console.log('Hello, World!');
+    console.log(parameterVariable);
+}
+
