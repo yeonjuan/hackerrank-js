36ada200688f53556101f54464e14d9a4f2d03d1
Heejun Lee <zaqq1414@gmail.com>
2019-06-30 23:50:41 +0900

    Arrow Function 적용

--- 10DaysOfJavascript/Day0/hello-world.js
+++ 10DaysOfJavascript/Day0/hello-world.js
@@ -1,6 +1,6 @@
 // https://www.hackerrank.com/challenges/js10-hello-world/problem
 
-function greeting(parameterVariable) {
+const greeting = (parameterVariable) => {
     console.log('Hello, World!');
     console.log(parameterVariable);
 }
