0eac0790a867576d46a5f912d07e448c646e2d01
Heejun Lee <zaqq1414@gmail.com>
2019-07-02 22:57:35 +0900

    resolve #3-arithmetic-operators 문제풀이 및 문서추가

---
+++ 10DaysOfJavascript/Day1/arithmetic-oprators.js
@@ -0,0 +1,13 @@
+// https://www.hackerrank.com/challenges/js10-arithmetic-operators/problem
+
+const getArea = (length, width) => {
+    let area = length * width;
+    return area;
+}
+
+const getPerimeter = (length, width) => {
+    let perimeter = 2 * (length + width);
+
+    return perimeter;
+}
+
