c9505c22ad2ea0375859dc200def38884ddbce78
yeonjuan <yeonjuan93@naver.com>
2019-07-03 23:43:50 +0900

    resolve #5-two-characters 문제풀이 및 문서 추가

---
+++ ProblemSolving/String/two-characters.js
@@ -0,0 +1,34 @@
+// https://www.hackerrank.com/challenges/two-characters/problem
+
+const alternate = (originString) => {
+  const chars = [...originString];
+  const uniques = [...new Set(chars)];
+  let max = 0;
+
+  forUniquePairs(uniques, (pair) => {
+    const alteredChars = chars.filter(char => pair.includes(char));
+    if (isValid(alteredChars)) {
+      max = Math.max(max, alteredChars.length);
+    }
+  });
+
+  return max;
+}
+
+const forUniquePairs = (array, fn) => {
+  const {length} = array;
+  for (let i = 0; i < length - 1; i ++) {
+    for (let j = i + 1; j < length; j ++) {
+        fn ([array[i], array[j]]);
+    }
+  }
+}
+
+const isValid = (array) => (
+  array.every((char, index, {length}) => {
+    if (index >= length - 1) {
+      return true;
+    }
+    return char !== array[index + 1];
+  })
+);
--- README.md
+++ README.md
@@ -25,3 +25,4 @@
 | String |[Two String](https://www.hackerrank.com/challenges/two-strings/problem) | [Solution](./ProblemSolving/String/two-strings.js) |
 | String |[Game of Thrones - I](https://www.hackerrank.com/challenges/game-of-thrones/problem) | [Solution](./ProblemSolving/String/game-of-thrones-1.js) |
 | String |[Pangrams](https://www.hackerrank.com/challenges/pangrams/problem) | [Solution](./ProblemSolving/String/pangrams.js) |
+| String |[Two Characters](https://www.hackerrank.com/challenges/two-characters/problem) | [Solution](./ProblemSolving/String/two-characters.js) |
