e615990ab0eec739c705eb7690e89eec973af449
Heejun Lee <zaqq1414@gmail.com>
2019-07-08 00:31:45 +0900

    refactor #25-문제에서 주어진 나머지 테스트코드 추가

--- 10DaysOfJavascript/Day1/let-and-const.js
+++ 10DaysOfJavascript/Day1/let-and-const.js
@@ -6,5 +6,14 @@ function main() {
 
     console.log((parseFloat(r) ** 2) * PI);
     console.log(2 * PI * parseFloat(r));
+
+		try {
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
 }
 
