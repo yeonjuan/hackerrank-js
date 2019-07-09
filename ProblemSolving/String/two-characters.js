0bf8918909e67dcaf584040d7419afcd062f073c
yeonjuan <yeonjuan93@naver.com>
2019-07-09 23:34:30 +0900

    refactor #5 린트 적용

--- ProblemSolving/String/two-characters.js
+++ ProblemSolving/String/two-characters.js
@@ -1,11 +1,11 @@
 // https://www.hackerrank.com/challenges/two-characters/problem
 
-const alternate = (originString) => {
+const alternate = originString => {
   const chars = [...originString];
   const uniques = [...new Set(chars)];
   let max = 0;
 
-  forUniquePairs(uniques, (pair) => {
+  forUniquePairs(uniques, pair => {
     const alteredChars = chars.filter(char => pair.includes(char));
     if (isValid(alteredChars)) {
       max = Math.max(max, alteredChars.length);
@@ -13,22 +13,23 @@ const alternate = (originString) => {
   });
 
   return max;
-}
+};
 
 const forUniquePairs = (array, fn) => {
   const {length} = array;
-  for (let i = 0; i < length - 1; i ++) {
-    for (let j = i + 1; j < length; j ++) {
-        fn ([array[i], array[j]]);
+  for (let i = 0; i < length - 1; i++) {
+    for (let j = i + 1; j < length; j++) {
+      fn([array[i], array[j]]);
     }
   }
-}
+};
 
-const isValid = (array) => (
+const isValid = array => (
   array.every((char, index, {length}) => {
     if (index >= length - 1) {
       return true;
     }
+
     return char !== array[index + 1];
   })
 );
