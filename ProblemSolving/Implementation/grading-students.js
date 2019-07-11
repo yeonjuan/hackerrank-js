f6b0e0534426cbcd16ad60516cebb03f3e466c62
yeonjuan <yeonjuan93@naver.com>
2019-07-11 21:50:15 +0900

    refactor #38-불필요한 parseInt 제거

--- ProblemSolving/Implementation/grading-students.js
+++ ProblemSolving/Implementation/grading-students.js
@@ -1,7 +1,7 @@
 // https://www.hackerrank.com/challenges/grading/problem
 
 const shouldRounded = grade => (grade % 5 >= 3) && (grade >= 38);
-const getRounded = grade => parseInt(Math.round(grade / 5) * 5, 10);
+const getRounded = grade => Math.round(grade / 5) * 5;
 
 const gradingStudents = grades => (
   grades.map(grade => shouldRounded(grade) ? getRounded(grade) : grade)
