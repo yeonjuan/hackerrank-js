7581d08b4b2cc56e25ec0328c109f73532623e97
Heejun Lee <zaqq1414@gmail.com>
2019-07-15 00:32:18 +0900

    refactor #45-정규표현식과 전개구문 적용

--- ProblemSolving/String/caesar-cipher.js
+++ ProblemSolving/String/caesar-cipher.js
@@ -1,31 +1,29 @@
 // https://www.hackerrank.com/challenges/caesar-cipher-1/problem
 
-const shouldGoFirst = (pre, shift) => {
-  const post = String.fromCharCode(pre.charCodeAt(0) + shift);
-
-  return (post > 'z') ? true : !!((pre <= 'Z' && post.charCodeAt(0) > 'Z'.charCodeAt(0)));
-};
-
-const isSymbol = letter => (letter >= 'A' && letter <= 'Z') ? false :
-  !((letter >= 'a' && letter <= 'z'));
-
 const caesarCipher = (letters, shift) => {
-  const {length} = letters;
-  shift = shift % 26;
+  const range = 'Z'.charCodeAt(0) - 'A'.charCodeAt(0) + 1;
+  const shiftMod = shift % range;
 
-  let ret = '';
+  const ret = [...letters].map(letter => {
+    if (/[a-z]/.test(letter)) {
+      let moved = letter.charCodeAt(0) + shiftMod;
+      if (moved > 'z'.charCodeAt(0)) {
+        moved = 'a'.charCodeAt(0) + (moved - 'z'.charCodeAt(0) - 1);
+      }
 
-  for (let i = 0; i < length; i++) {
-    const cur = letters.charAt(i);
-    if (isSymbol(cur)) {
-      ret += cur;
-    } else if (shouldGoFirst(cur, shift)) {
-      const moved = (cur <= 'Z') ? (String.fromCharCode('A'.charCodeAt(0) + (cur.charCodeAt(0) + shift - 'Z'.charCodeAt(0)) - 1)) : String.fromCharCode('a'.charCodeAt(0) + (cur.charCodeAt(0) + shift - 'z'.charCodeAt(0)) - 1);
-      ret += moved;
-    } else {
-      ret += String.fromCharCode(cur.charCodeAt(0) + shift);
+      return String.fromCharCode(moved);
     }
-  }
+    if (/[A-Z]/.test(letter)) {
+      let moved = letter.charCodeAt(0) + shiftMod;
+      if (moved > 'Z'.charCodeAt(0)) {
+        moved = 'A'.charCodeAt(0) + (moved - 'Z'.charCodeAt(0) - 1);
+      }
 
-  return ret;
+      return String.fromCharCode(moved);
+    }
+
+    return letter;
+  });
+
+  return ret.join('');
 };
