03d7712d0d8fa99453afdb2a19416a08bd3a75dd
yeonjuan <yeonjuan93@naver.com>
2019-07-15 20:45:28 +0900

    refactor #46-lint 스타일 적용

--- 10DaysOfJavascript/Day2/loops.js
+++ 10DaysOfJavascript/Day2/loops.js
@@ -4,12 +4,12 @@ const isVowel = letter => /^a|e|i|o|u/.test(letter);
 const isConsonant = letter => !isVowel(letter);
 
 const vowelsAndConsonants = letters => {
-  [...letters].forEach((letter) => {
+  [...letters].forEach(letter => {
     if (isVowel(letter)) {
       console.log(letter);
     }
   });
-  [...letters].forEach((letter) => {
+  [...letters].forEach(letter => {
     if (isConsonant(letter)) {
       console.log(letter);
     }
