e868af6fa89464ccd4b164221ccc1ecb3a1c44e1
bumkeyy <kgbum2222@gmail.com>
2019-07-15 23:37:21 +0900

    refactor #55-conditional-statements-if-else Lint에 맞게 수정 및 예외처리 코드 제거

--- 10DaysOfJavascript/Day2/conditional-statements-if-else.js
+++ 10DaysOfJavascript/Day2/conditional-statements-if-else.js
@@ -1,24 +1,21 @@
-//https://www.hackerrank.com/challenges/js10-if-else/problem
+//  https://www.hackerrank.com/challenges/js10-if-else/problem
 
-function getGrade(score) {
-    let grade;
-    // Write your code here
-    if (0 <= score && score <= 5) {
-        grade = 'F';
-    } else if (score <= 10) {
-        grade = 'E';
-    } else if (score <= 15) {
-        grade = 'D';
-    } else if (score <= 20) {
-        grade = 'C';
-    } else if (score <= 25) {
-        grade = 'B';
-    } else if (score <= 30) {
-        grade = 'A';
-    } else {
-        console.log("error");
-        return;
-    }
-    
-    return grade;
-}
+const getGrade = score => {
+  let grade;
+  if (score <= 5) {
+    grade = 'F';
+  } else if (score <= 10) {
+    grade = 'E';
+  } else if (score <= 15) {
+    grade = 'D';
+  } else if (score <= 20) {
+    grade = 'C';
+  } else if (score <= 25) {
+    grade = 'B';
+  } else if (score <= 30) {
+    grade = 'A';
+  }
+
+  return grade;
+};
+
