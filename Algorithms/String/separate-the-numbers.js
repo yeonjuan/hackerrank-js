40b409d8f4cb64232cd36d1954848601428295f8
Heejun Lee <zaqq1414@gmail.com>
2019-08-07 23:23:43 +0900

    refactor #123-complexity 감소 및 ESLint disable 적용(BigInt)

--- Algorithms/String/separate-the-numbers.js
+++ Algorithms/String/separate-the-numbers.js
@@ -4,8 +4,6 @@
  * @link https://www.hackerrank.com/challenges/separate-the-numbers/problem
  */
 
-const getDiff = (value1, value2) => value2 - value1;
-
 const digitCheck = (digit, value) => {
   let ret = 1;
   let mod = Number(value);
@@ -17,36 +15,43 @@ const digitCheck = (digit, value) => {
   return digit === ret;
 };
 
+const isPossible = (query, digit) => {
+  const {length} = query;
+  let idx = 0;
+  let curDigit = digit;
+  let curValue = BigInt(query.substr(idx, curDigit)); // eslint-disable-line
+  idx += curDigit;
+  let nextDigit = curDigit;
+  let nextValue = BigInt(query.substr(idx, nextDigit)); // eslint-disable-line
+
+  while (idx < length) {
+    if (!digitCheck(nextDigit, nextValue)) {
+      break;
+    }
+
+    const diff = Number(nextValue - curValue);
+    if (diff === 1) {
+      curValue = nextValue;
+      curDigit = nextDigit;
+      idx += nextDigit;
+      nextValue = BigInt(query.substr(idx, nextDigit)); // eslint-disable-line
+    } else if (nextDigit - curDigit === 0) {
+      nextDigit++;
+      nextValue = BigInt(query.substr(idx, nextDigit)); // eslint-disable-line
+    } else {
+      break;
+    }
+  }
+
+  return (idx >= length);
+};
+
 // Complete the separateNumbers function below.
 const separateNumbers = query => {
   const {length} = query;
   for (let digit = 1; digit <= length / 2; digit++) {
-    let idx = 0;
-    let curDigit = digit;
-    let curValue = BigInt(query.substr(idx, curDigit)); // eslint-disable-line
-    idx += curDigit;
-    let nextDigit = curDigit;
-    let nextValue = BigInt(query.substr(idx, nextDigit)); // eslint-disable-line
-
-    while (idx < length) {
-      if (!digitCheck(nextDigit, nextValue)) {
-        break;
-      }
-      const diff = Number(nextValue - curValue);
-      if (diff === 1) {
-        curValue = nextValue;
-        curDigit = nextDigit;
-        idx += nextDigit;
-        nextValue = BigInt(query.substr(idx, nextDigit)); // eslint-disable-line
-      } else if (nextDigit === curDigit) {
-        nextDigit++;
-        nextValue = BigInt(query.substr(idx, nextDigit)); // eslint-disable-line
-      } else {
-        break;
-      }
-    }
-    if (idx >= length) {
-      console.log('YES', query.substr(0, digit));
+    if (isPossible(query, digit)) {
+      console.log('YES', query.substring(0, digit));
 
       return;
     }
