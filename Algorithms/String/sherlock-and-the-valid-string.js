a718be55f9967cec74211865b2f3a7fa1b90fa6a
yeonjuan <yeonjuan@yeonjuanui-MacBook-Pro.local>
2019-08-11 02:32:59 +0900

    resolve #131-sherlock and the valid string 문제 풀이

---
+++ Algorithms/String/sherlock-and-the-valid-string.js
@@ -0,0 +1,30 @@
+const countChars = chars => chars.reduce((counter, char) => {
+  counter[char] = (counter[char] || 0) + 1;
+
+  return counter;
+}, {});
+
+const hasEqual = (count, idx, [initial]) => count === initial;
+
+const isValid = str => {
+  const counts = Object.values(countChars([...str]));
+  const min = Math.min.apply(null, counts);
+  const mins = counts.filter(count => count === min);
+  const biggers = counts.filter(count => count > min);
+
+  if (counts.every(hasEqual)) {
+    return 'YES';
+  }
+
+  if (mins.length === 1
+    && biggers.every(hasEqual)) {
+    return 'YES';
+  }
+
+  if (biggers.length === 1
+    && biggers.every(bigger => bigger - min === 1)) {
+    return 'YES';
+  }
+
+  return 'NO';
+};
